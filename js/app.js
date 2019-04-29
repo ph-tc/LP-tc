3817c90943dcca5453cbe8a777d5b296c188a5f0
philalexeev <phill08@mail.ru>
2019-04-29 13:42:09 +0300

    finish adaptive + add smooth scrolling

--- js/app.js
+++ js/app.js
@@ -1,5 +1,24 @@
+// Scroll initialization
+
+var scroll = new SmoothScroll('a[href*="#"]');
+
+
+// Page functionality
+
 var burger = document.querySelector('.hamburger');
+var menu = document.querySelector('.section-1 ul');
 
 burger.addEventListener('click', function() {
   burger.classList.toggle('is-active');
+  menu.classList.toggle('open');
+  document.body.classList.toggle('blocked');
 });
+
+menu.addEventListener('click', function(e) {
+  var target = e.target;
+  if ( target.tagName === 'A' ) {
+    menu.classList.remove('open');
+    burger.classList.remove('is-active');
+    document.body.classList.remove('blocked');
+  }
+})
